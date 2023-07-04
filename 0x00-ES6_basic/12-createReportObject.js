27b77cc3baaecbee8dd20d924a1b8eac47e926cf
Motaz-Mukhtar <motazksa2@gmail.com>
2023-07-04 01:07:45 +0000

    Add all Project Tasks.

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+export default function createReportObject(employeesList) {
+  return {
+	  allEmployees: ...emplyeesList,
+	  getNumberOfDepartments: (list) => {
+		  return list.length;
+	  },
+  };
+}
---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,3 @@
+export default function returnHowManyArguments(...argArr) {
+	return argArr.length
+}
