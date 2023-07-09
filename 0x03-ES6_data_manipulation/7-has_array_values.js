b05d1ab389ba309e27cef3be68ec42b56926d7c3
Motaz-Mukhtar <motazksa2@gmail.com>
2023-07-09 02:02:41 +0000

    eslit fix 7-has_array_values.js file

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,10 +1,8 @@
 export default function hasValuesFromArray(set, array) {
   let bool = false;
-  for (let i of array) {
-    if (set.has(i))
-      bool = true;
-    else
-      bool = false;
+  for (const i of array) {
+    if (set.has(i)) bool = true;
+    else bool = false;
   }
   return bool;
 }
