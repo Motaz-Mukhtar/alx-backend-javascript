8ea99920f48518a250e74c5a1a38ee57d7cbfd85
Motaz-Mukhtar <motazksa2@gmail.com>
2023-08-24 08:50:49 +0200

    Update /0x06-unittests_in_js/0-calcul.test.js file

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,7 +4,11 @@ const calculateNumber = require('./0-calcul.js');
 describe('calculateNumber', function () {
   it('should return sum of two numbers.', function () {
     assert.equal(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(5, 2), 7);
+  });
+
+  it('should return sum of round numbers', function () {
     assert.equal(calculateNumber(1.2, 3.7), 5);
     assert.equal(calculateNumber(1.5, 3.7), 6);
-  });
+  })
 });
