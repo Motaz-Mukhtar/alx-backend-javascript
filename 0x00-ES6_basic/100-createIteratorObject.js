43832e0478aec934109db8f281f693de12ea5837
Motaz-Mukhtar <mohammedmuk20@gmail.com>
2023-09-26 17:14:37 +0200

    ESlint Validtion fix at 0x00-ES6_basic

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,7 @@
 export default function createIteratorObject(report) {
   const arr = [];
 
-  for (let dep of Object.keys(report.allEmployees)) {
+  for (const dep of Object.keys(report.allEmployees)) {
     arr.push(...report.allEmployees[dep]);
   }
 
